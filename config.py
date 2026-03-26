46514b041d15e95919b4de7798bcbb6216f7ca0c
sirui-cell <yongqiqingting@gmail.com>
2026-03-26 13:37:13 +0800

    Update config.py
    update url

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-url_pump = "https://a"
+BOT_TOKEN = "your_token"
+CHAT_ID = -1002604809472
+url_pump = "https://pump"
+url_moon = "https://moon"
+url_bonk = "https://bonk"
+url_1h = "https://1h"
+url_24 = "https://24"
