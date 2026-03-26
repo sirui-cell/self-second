ac7aec6aa0152d9a85f39ad54974c4010844c85a
sirui-cell <yongqiqingting@gmail.com>
2026-03-26 13:41:34 +0800

    Update config.py
    update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ CHAT_ID = -1002604809472
 url_pump = "https://pump"
 url_moon = "https://moon"
 url_bonk = "https://bonk"
-url_1h = "https://1h"
-url_24 = "https://24"
+url_1h = "https://gmgn.ai/api/v1/rank/sol/swaps/1h?device_id=3e417959-0b07-4169-881d-2e2beb11791f&fp_did=2e0207aac1641053e8c014ed0945c9d0&client_id=gmgn_web_20260306-11434-42d387e&from_app=gmgn&app_ver=20260306-11434-42d387e&tz_name=Asia%2FShanghai&tz_offset=28800&app_lang=zh-CN&os=web&worker=0&orderby=creation_timestamp&direction=desc&filters[]=renounced&filters[]=frozen"
+url_24 = "https://gmgn.ai/api/v1/rank/sol/swaps/24h?device_id=3e417959-0b07-4169-881d-2e2beb11791f&fp_did=2e0207aac1641053e8c014ed0945c9d0&client_id=gmgn_web_20260306-11434-42d387e&from_app=gmgn&app_ver=20260306-11434-42d387e&tz_name=Asia%2FShanghai&tz_offset=28800&app_lang=zh-CN&os=web&worker=0&orderby=creation_timestamp&direction=desc&filters[]=renounced&filters[]=frozen"
